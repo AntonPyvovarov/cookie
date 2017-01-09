e773599d5f2f39da1ac047aed461fadc82533801
AntonPyvovarov <Antopyvo87@gmail.com>
2017-01-09 22:29:22 +0200

    delete some stings

--- flow.php
+++ flow.php
@@ -5,13 +5,6 @@ $loggedUser=false;
 $visits=isset($_COOKIE ['visits'])?$_COOKIE ['visits']:0;
 $visits++;
 setcookie('visits',$visits);
-//choose background color
-/*if(isset($_POST['action'])&& $_POST['action']=='changeColor'){
-    setcookie('color',$_POST['color']);
-    $_COOKIE['color']=$_POST['color'];
-}
-$color=isset($_COOKIE['color']) ? $_COOKIE['color'] : 'red'; //add inspection for empty string
-*/
 if (isset($_POST['action'])&& $_POST ['action']=='login'){
     $loggedUser=isValidlogin($_POST['login'], $_POST['password']);
 }
