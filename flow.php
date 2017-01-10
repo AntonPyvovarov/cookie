97336060c39eba0f9b70acdeeecdec91198f2d71
AntonPyvovarov <Antopyvo87@gmail.com>
2017-01-10 22:58:52 +0200

    add time cookie

--- flow.php
+++ flow.php
@@ -5,23 +5,14 @@ $loggedUser=false;
 $visits=isset($_COOKIE ['visits'])?$_COOKIE ['visits']:0;
 $visits++;
 setcookie('visits',$visits);
-//choose background color
-/*if(isset($_POST['action'])&& $_POST['action']=='changeColor'){
-    setcookie('color',$_POST['color']);
-    $_COOKIE['color']=$_POST['color'];
-}
-$color=isset($_COOKIE['color']) ? $_COOKIE['color'] : 'red'; //add inspection for empty string
-*/
 if (isset($_POST['action'])&& $_POST ['action']=='login'){
     $loggedUser=isValidlogin($_POST['login'], $_POST['password']);
 }
 
 if (isset($_POST['action'] )&& isset($_POST['action']) ){
-    setcookie('setColor',$_POST['action']);
+    setcookie('setColor',$_POST['action'],time()+60*5);
     $_COOKIE['setColor']=$_POST['action'];
 }
-
-
 $color=isset($_COOKIE['setColor'])?$_COOKIE['setColor']:'green';
 echo $color;
 
